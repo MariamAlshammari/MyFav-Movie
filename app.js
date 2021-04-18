6ae1920085f136d6d0861ec2946fd521df98cf4a
MariamAlshammar <>
2021-04-18 11:45:07 +0000

    function JS

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
  
-<<<<<<< HEAD
 var disneyPrincessname;
 
 var favMov= function(){
@@ -7,15 +6,9 @@ var favMov= function(){
 disneyPrincessname = prompt('Hello, please enter your fav disney Princess name cinderella or snow white !');
 
 
-=======
-var disneyPrincessname = prompt('Hello, please enter your fav disney Princess name cinderella or snow white !');
->>>>>>> 25e81968a3af3aece6cf91403cd9946017dfb91e
-
-
 while(disneyPrincessname !== 'cinderella'&& disneyPrincessname !== 'snow white'){
   disneyPrincessname = prompt('Please enter the name cinderella  or snow white');
 }
-<<<<<<< HEAD
 }
 
 favMov();
@@ -38,20 +31,5 @@ for(var i=0 ; i < imagesNumber; i++){
 }
 }
 showImg();
-=======
-
-var userChoose;
-
-if(disneyPrincessname === 'cinderella'){
-userChoose='<img src="https://static0.srcdn.com/wordpress/wp-content/uploads/2019/11/Cinderella.jpg">';
-}else if (disneyPrincessname === 'snow white'){
-  userChoose='<img src="https://e7.pngegg.com/pngimages/606/974/png-clipart-snow-white-princess-aurora-seven-dwarfs-disney-princess-belle-snow-white-seven-dwarfs-disney-princess.png">';
-}
 
 
-var imagesNumber= prompt('How many image do you want???');
-
-for(var i=0 ; i < imagesNumber; i++){
-  document.write(userChoose);
-}
->>>>>>> 25e81968a3af3aece6cf91403cd9946017dfb91e
